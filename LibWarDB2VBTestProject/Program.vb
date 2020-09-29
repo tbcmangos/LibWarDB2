a2d94294b5aa2e771a8030cd647ef57e6ba183fb
z <z@test.com>
2020-09-29 15:40:44 +0800

    this is how we read the string, but how do we split or which column do they belong

--- LibWarDB2VBTestProject/Program.vb
+++ LibWarDB2VBTestProject/Program.vb
@@ -1,4 +1,5 @@
 Imports System
+Imports System.Text
 Imports LibWarDB2VB.LibWarDB2
 Imports LibWarDB2VB.LibWarDB2.Structures
 
@@ -15,7 +16,13 @@ Module Program
         'Next
 
         'test change id
+
         wdc.DataSections(0).IdList(0) = 12
+
+        Dim i As Integer = wdc.SectionHeaders(0).StringTableSize
+        Dim sChar As Byte() = wdc.DataSections(0).StringData
+        Dim s = Encoding.UTF8.GetString(sChar) 'vbNullChar & vbNullChar & "小型" & vbNullChar & "中型" & vbNullChar & "大型" & vbNullChar
+
         wdc.Save("GarrPlotUICategory.db2")
     End Sub
 End Module
