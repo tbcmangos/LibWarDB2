c702826ff81a783a8bef3ae185ebe56e048e6d8a
z <z@test.com>
2020-09-29 15:24:39 +0800

    looks like we can modify the data now

--- LibWarDB2VBTestProject/Program.vb
+++ LibWarDB2VBTestProject/Program.vb
@@ -8,6 +8,14 @@ Module Program
         Dim wdc = New WDC3()
 
         wdc.Load("GarrPlotUICategory.db2")
+        ''might have multiple sections?
+        'Dim sec As Section() = wdc.DataSections
+        'For Each id In sec(0).IdList
+
+        'Next
+
+        'test change id
+        wdc.DataSections(0).IdList(0) = 12
         wdc.Save("GarrPlotUICategory.db2")
     End Sub
 End Module
